0b1a552e44dd5a95436c1878419b32efb8689eed
AlanShelbyCode <abiturientweselski@gmail.com>
2023-01-31 23:05:10 +0200

    JS_1

--- JavaScript/JS_1/main.js
+++ JavaScript/JS_1/main.js
@@ -1,3 +1,19 @@
 console.log("123");
 let i = 10;
 document.write(i);
+let arr = [
+    'hello','ouw','com','ua',1,
+    10,-999,123,3.14,2.7,16, true,false
+]
+console.log(arr);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+prompt(a);
+prompt(b);
+prompt(c);
