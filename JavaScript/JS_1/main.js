e51fd04f655cf366e6cb267322b7e3f8e36ea2e4
AlanShelbyCode <abiturientweselski@gmail.com>
2023-02-01 20:20:21 +0200

    JS_1

--- JavaScript/JS_1/main.js
+++ JavaScript/JS_1/main.js
@@ -7,12 +7,24 @@ let arr = [
 ]
 console.log(arr);
 
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//     З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 let a = 100;
 let b = '100';
 let c = true;
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
+
+let all = a +''+ b + ''+c + '';
+console.log(all)
+let all1 = `${a} ${b} ${c} `;
+console.log(all)
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
 
 prompt(a);
 prompt(b);
