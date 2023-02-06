494f04dce2ff5125081fa143067c91e3a7c4925d
AlanShelbyCode <abiturientweselski@gmail.com>
2023-02-06 20:25:29 +0200

    JS_2_if2

--- JavaScript/JS_2/main.js
+++ JavaScript/JS_2/main.js
@@ -11,12 +11,7 @@ let bookReader = {
 
 
 };
-let arr1 = [
-    1,2,3,4,5,true,'qwe',"qwe",8,9
-];
-for (let i = 0; i < arr1.length; i++) {
-    console.log(arr1[i]);
-}
+
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
 let bookReader1 = {
     title : 'haryy',
@@ -62,17 +57,30 @@ console.log(bookReader6);
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 //
 
-let masyw =  [
-    name,
-    username,
-    password
+let masyw =  [{
+        title: 'haryy',
+        pageCount: 15,
+        genre: 'male',
+        authors: ['egor gtin', 16]
+    },
+    {
+        title: 'Potter',
+        pageCount: 15,
+        genre: 'male',
+        authors: ['egor gtin', 16]
+    },
+    {
+        title: 'Ricki',
+        pageCount: 15,
+        genre: 'male',
+        authors: ['egor gtin', 16]
+    },
 ];
-let masyw =  [
-    name,
-    username,
-    password
-];
+console.log(masyw[0].title);
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється  масивом. Кожен автор має поля name та age.
+
+
+
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 //
 
@@ -81,9 +89,49 @@ let masyw =  [
 // Логічні розгалуження:
 //     - Є змінна х, якій ви надаєте довільне числове значення.
 //     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+let x = 1453;
+
+if( x!== 0){
+    console.log('Wirno');
+}
+else {
+    console.log('Ne wirno');
+}
+
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
+time = 16;
+if(time>0 && time<=15 ){
+    console.log('1');
+}
+else if(time>15 && time<=30 ){
+    console.log('2');
+}
+
+else if(time>30 && time<=45 ){
+    console.log('3');
+}
+
+else if(time>45 && time<=60 ){
+    console.log('4');
+}
+else {
+    console.log('Defined time is undefind');
+}
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+let day = 10;
+if (0<day<=10){
+    console.log('1');
+}
+else if (10<day<=20){
+    console.log('1');
+}
+else if (20<day<=31){
+    console.log('1');
+}
+else{
+    'deklarate errors items';
+}
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
