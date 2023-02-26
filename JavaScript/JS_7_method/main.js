600f98f16a3e3bf09f81c99159ede27baa6ddf83
AlanShelbyCode <abiturientweselski@gmail.com>
2023-02-26 23:40:31 +0200

    JS_7_short_hand

---
+++ JavaScript/JS_7_method/main.js
@@ -0,0 +1,45 @@
+let koloda = [
+    {title:6,index:'serce'},
+    {title:7,index:'serce'},
+    {title:8,index:'serce'},
+    {title:9,index:'serce'},
+    {title:10,index:'serce'},
+    {title:'valet',index:'serce'},
+    {title:'dama',index:'serce'},
+    {title:'korol',index:'serce'},
+    {title:'tuz',index:'serce'},
+
+    {title:6,index:'buba'},
+    {title:7,index:'buba'},
+    {title:8,index:'buba'},
+    {title:9,index:'buba'},
+    {title:10,index:'buba'},
+    {title:'valet',index:'buba'},
+    {title:'dama',index:'buba'},
+    {title:'korol',index:'buba'},
+    {title:'tuz',index:'buba'},
+
+    {title:6,index:'pika'},
+    {title:7,index:'pika'},
+    {title:8,index:'pika'},
+    {title:9,index:'pika'},
+    {title:10,index:'pika'},
+    {title:'valet',index:'pika'},
+    {title:'dama',index:'pika'},
+    {title:'korol',index:'pika'},
+    {title:'tuz',index:'pika'},
+
+    {title:6,index:'chresta'},
+    {title:7,index:'chresta'},
+    {title:8,index:'chresta'},
+    {title:9,index:'chresta'},
+    {title:10,index:'chresta'},
+    {title:'valet',index:'chresta'},
+    {title:'dama',index:'chresta'},
+    {title:'korol',index:'chresta'},
+    {title:'tuz',index:'chresta'},
+
+]
+
+let filter = {title};
+console.log(filter);
